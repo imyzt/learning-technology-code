c812da61982549f34e0feea24ad715a015a00ba7
imyzt <imyzt01@gmail.com>
2019-05-18 13:18:38 +0800

    NIO study

---
+++ nio-chat-room/src/main/java/top/imyzt/nio/chat/NioServer.java
@@ -0,0 +1,96 @@
+package top.imyzt.nio.chat;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.Charset;
+import java.util.Iterator;
+import java.util.Set;
+
+/**
+ * @author imyzt
+ * @date 2019/5/18
+ * @description NIO 服务端
+ */
+public class NioServer {
+
+    /**
+     * 启动方法
+     */
+    public void start() throws IOException {
+
+        // 1. 创建Selector
+        Selector selector = Selector.open();
+
+        // 2. 根据ServerSocketChannel创建channel通道
+        ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+
+        // 3. 为channel绑定端口
+        serverSocketChannel.bind(new InetSocketAddress(8000));
+
+        // 4. 设置channel为非阻塞式IO
+        serverSocketChannel.configureBlocking(false);
+
+        // 5. 将channel注册到selector上, 监听连接事件
+        serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+
+        // 6. 循环等待新接入的连接
+        for (;;) {
+
+            // 获取可用的channel数量
+            int readyChannels = selector.select();
+
+            if (readyChannels == 0) {
+                continue;
+            }
+
+            // 获取可用的channel集合
+            Set<SelectionKey> selectionKeys = selector.selectedKeys();
+            Iterator<SelectionKey> iterator = selectionKeys.iterator();
+            while (iterator.hasNext()) {
+
+                // selectionKey实例
+                SelectionKey selectionKey = iterator.next();
+
+                // 移除Set中当前的selectionKey
+                iterator.remove();
+
+                // 7. 根据就绪状态, 调用对应的处理业务逻辑
+
+                // 如果是 接入事件
+                // do somethings
+
+                // 如果是 可读事件
+                // do somethings
+            }
+        }
+    }
+
+    /**
+     * 接入事件处理器
+     */
+    private void acceptHandler(ServerSocketChannel serverSocketChannel,
+                               Selector selector)
+            throws IOException {
+
+        // 如果要是接入事件, 创建SocketChannel
+        SocketChannel socketChannel = serverSocketChannel.accept();
+
+        // 将SocketChannel设置为非阻塞状态
+        socketChannel.configureBlocking(false);
+
+        // 将SocketChannel注册到selector中
+        socketChannel.register(selector, SelectionKey.OP_READ);
+
+        // 回复客户端信息
+        socketChannel.write(Charset.forName("UTF-8")
+                .encode("你与聊天室里其他成员都不是朋友关系, 请注意隐私安全"));
+    }
+
+    public static void main(String[] args) {
+        NioServer nioServer = new NioServer();
+    }
+}
