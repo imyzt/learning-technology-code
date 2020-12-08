9d4a01835c96d3daa85606a3ab6d298c26c633b1
imyzt <imyzt01@gmail.com>
2020-12-08 22:37:02 +0800

    spring boot management.endpoints

---
+++ source-code/xiaomage-springboot-teach/production-ready/src/main/java/top/imyzt/learing/productionready/ProductionReadyApplication.java
@@ -0,0 +1,16 @@
+package top.imyzt.learing.productionready;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductionReadyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductionReadyApplication.class, args);
+    }
+    
+    
+    
+
+}
