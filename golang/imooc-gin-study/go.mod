2b28622be5fb45ff8f768e074fe6dcf70af94628
imyzt <imyzt01@gmail.com>
2021-03-07 23:27:24 +0800

    gin 参数验证与validator/v10自定义验证

--- golang/imooc-gin-study/go.mod
+++ golang/imooc-gin-study/go.mod
@@ -4,7 +4,7 @@ go 1.15
 
 require (
 	github.com/gin-gonic/gin v1.6.3
-	github.com/go-playground/validator/v10 v10.4.1 // indirect
+	github.com/go-playground/validator/v10 v10.4.1
 	github.com/golang/protobuf v1.4.3 // indirect
 	github.com/json-iterator/go v1.1.10 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
---
+++ golang/imooc-gin-study/valid_custom/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/validator/v10"
+	"net/http"
+	"time"
+)
+
+/**
+更多验证规则
+https://pkg.go.dev/gopkg.in/bluesuncorp/validator.v8#section-documentation
+*/
+type Booking struct {
+	// 时间必须符合bookabledate检查
+	ChinkIn time.Time `form:"checkIn" json:"chink_in" binding:"required,bookabledate" time_format:"2006-01-02"`
+	// 时间必须大于ChinkIn
+	ChinkOut time.Time `form:"checkOut" json:"chink_out" binding:"required,gtfield=ChinkIn" time_format:"2006-01-02"`
+}
+
+func bookableDate(fl validator.FieldLevel) bool {
+
+	if date, ok := fl.Field().Interface().(time.Time); ok {
+		// 大于今天
+		return date.Unix() > time.Now().Unix()
+	}
+	return false
+}
+
+func main() {
+
+	r := gin.Default()
+
+	// 注册验证规则
+	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+		err := v.RegisterValidation("bookabledate", bookableDate)
+		if err != nil {
+			for _, e := range err.(validator.ValidationErrors) {
+				fmt.Println(e)
+			}
+		}
+	}
+
+	r.POST("test", func(context *gin.Context) {
+
+		var booking Booking
+		// 根据不同的context-type,做不同的映射
+		if err := context.ShouldBind(&booking); err != nil {
+			context.String(http.StatusBadRequest, "booking bind error %v", err.Error())
+			return
+		}
+
+		context.JSON(http.StatusOK, booking)
+	})
+
+	r.Run()
+
+}
