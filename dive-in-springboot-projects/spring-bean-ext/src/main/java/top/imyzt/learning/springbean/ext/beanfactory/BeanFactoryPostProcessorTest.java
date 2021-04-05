f7417603cb5d0553e0aea8d8413adf921c1f919e
imyzt <imyzt01@gmail.com>
2021-04-05 15:59:44 +0800

    BeanFactoryPostProcessor的周期

---
+++ dive-in-springboot-projects/spring-bean-ext/src/main/java/top/imyzt/learning/springbean/ext/beanfactory/BeanFactoryPostProcessorTest.java
@@ -0,0 +1,34 @@
+package top.imyzt.learning.springbean.ext.beanfactory;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import top.imyzt.learning.springbean.ext.common.Red;
+
+/**
+ * @author imyzt
+ * @date 2021/04/05
+ * @description BeanFactoryPostProcessor
+ */
+@Configuration
+@ComponentScan("top.imyzt.learning.springbean.ext.beanfactory")
+public class BeanFactoryPostProcessorTest {
+
+    public static void main(String[] args) {
+
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(BeanFactoryPostProcessorTest.class);
+
+        context.close();
+
+        //共有bean定义: 8
+        //bean定义: [org.springframework.context.annotation.internalConfigurationAnnotationProcessor, org.springframework.context.annotation.internalAutowiredAnnotationProcessor, org.springframework.context.annotation.internalCommonAnnotationProcessor, org.springframework.context.event.internalEventListenerProcessor, org.springframework.context.event.internalEventListenerFactory, beanFactoryPostProcessorTest, myBeanFactoryPostProcessor, red]
+        //15:22:17.031 [main] DEBUG org.springframework.beans.factory.support.DefaultListableBeanFactory - Creating shared instance of singleton bean 'red'
+        //red constructor...
+    }
+
+    @Bean
+    public Red red() {
+        return new Red();
+    }
+}
