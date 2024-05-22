180ed4ad659497460e99d2c3ee0068b3ca565885
imyzt <imyzt01@gmail.com>
2024-05-22 21:57:06 +0800

    spring 6.2 async startup

--- dive-in-springboot-projects/spring-async-startup/src/main/java/top/imyzt/learning/spring/startup/config/RegisterConfiguration.java
+++ dive-in-springboot-projects/spring-async-startup/src/main/java/top/imyzt/learning/spring/startup/config/RegisterConfiguration.java
@@ -6,6 +6,11 @@ import org.springframework.context.annotation.Configuration;
 import top.imyzt.learning.spring.startup.core.TestABean;
 import top.imyzt.learning.spring.startup.core.TestBBean;
 
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
 /**
  * @author imyzt
  * @date 2024/05/21
@@ -14,14 +19,18 @@ import top.imyzt.learning.spring.startup.core.TestBBean;
 @Configuration
 public class RegisterConfiguration {
 
-    @Bean
+    @Bean(bootstrap = Bean.Bootstrap.BACKGROUND)
     public TestABean testABean() {
         return new TestABean();
     }
 
-    @Bean
+    @Bean(bootstrap = Bean.Bootstrap.BACKGROUND)
     public TestBBean testBBean() {
         return new TestBBean();
     }
 
+    @Bean()
+    public Executor bootstrapExecutor() {
+        return new ThreadPoolExecutor(2, 10, 1, TimeUnit.MINUTES, new ArrayBlockingQueue<>(1024));
+    }
 }
