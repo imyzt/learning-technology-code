b35c4d26680701c7980d9bbb976f38037302fc59
imyzt <imyzt01@gmail.com>
2024-06-02 23:13:19 +0800

    cds

---
+++ framework-in-java/java12-new-futures-cds/src/main/java/top/imyzt/learning/cds/Java12NewFuturesCdsApplication.java
@@ -0,0 +1,13 @@
+package top.imyzt.learning.cds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java12NewFuturesCdsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Java12NewFuturesCdsApplication.class, args);
+    }
+
+}
