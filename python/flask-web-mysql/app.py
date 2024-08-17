8ee4f42b23c537c6c3a48f42f7b077e590ab5351
imyzt <imyzt01@gmail.com>
2024-08-17 14:39:05 +0800

    add cors

--- python/flask-web-mysql/app.py
+++ python/flask-web-mysql/app.py
@@ -2,11 +2,13 @@ import socket
 
 from apscheduler.schedulers.background import BackgroundScheduler
 from flask import Flask
+from flask_cors import CORS
 
 from job import schedule
 from log import log
 
 app = Flask(__name__)
+CORS(app)
 
 # 导入路由模块并注册 Blueprint，必须放在主文件，否则gunicorn无法加载到路由
 from routes import main_blueprint
