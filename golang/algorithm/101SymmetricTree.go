d8b3547d7e75c5f86e2a10c89f0e442ad1907358
imyzt <imyzt01@gmail.com>
2024-11-02 08:58:01 +0800

    leetcode

--- golang/algorithm/101SymmetricTree.go
+++ golang/algorithm/101SymmetricTree.go
@@ -1,21 +1,21 @@
 package main
 
+// 镜像对称二叉树
 func isSymmetric(root *TreeNode) bool {
 	return root == nil || isSymmetric1(root.Left, root.Right)
 }
 
-// 镜像对称二叉树
-func isSymmetric1(p *TreeNode, q *TreeNode) bool {
-	if p == nil && q == nil {
-		return true
+func isSymmetric1(left *TreeNode, right *TreeNode) bool {
+	// 是否存在一边为空的情况,如果有一边为空另一边不是则返回false
+	if left == nil || right == nil {
+		return left == nil && right == nil
 	}
-	if p == nil || q == nil {
+	// 对比两个相对位置是否值相等
+	if left.Val != right.Val {
 		return false
 	}
-	if p.Val != q.Val {
-		return false
-	}
-	return isSymmetric1(p.Left, q.Right) && isSymmetric1(p.Right, q.Left)
+	// 左树的左节点和右树的右节点, 左树的右节点和右树的左节点进行递归对比
+	return isSymmetric1(left.Left, right.Right) && isSymmetric1(left.Right, right.Left)
 }
 
 //    1
